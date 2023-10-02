30f8b20c4724f112f8dcf437e0b68d9fa476a5f9
Mila-Jo Davies <daviesmila@gmail.com>
2023-10-02 15:05:38 +0200

    insignificant changes

--- js/bookings.js
+++ js/bookings.js
@@ -1,9 +1,14 @@
 const troll = document.getElementById('lol');
 const lastLaugh = document.createElement('div');
-lastLaugh.textContent = 'YOUR APPLICATION HAS BEEN DELETED SUCCESSFULLY';
-lastLaugh.style.color = 'red';
-lastLaugh.style.fontWeight = 'bold';
+const lineBreak = document.createElement('br');
 
+lastLaugh.textContent = 'YOUR APPLICATION HAS BEEN DELETED SUCCESSFULLY';
+lastLaugh.style.color = 'green';
+lastLaugh.style.fontWeight = 'bold';
+lastLaugh.style.fontSize = 'xx-large';
+
+
+document.getElementById('last-laugh').appendChild(lineBreak);
 document.getElementById('last-laugh').appendChild(lastLaugh);
 
 troll.addEventListener('click', () => {
