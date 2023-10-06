909de00a6cc5a99114cd26c9c007875f24cea6a5
Mila-Jo Davies <daviesmila@gmail.com>
2023-10-06 12:44:05 +0200

    fixed animations

--- js/about.js
+++ js/about.js
@@ -1,14 +1,14 @@
+// Get a reference to the container where you want to insert the new element
 const container = document.getElementById('egg');
 const clickRatL = document.getElementById('big-rat');
 const clickRatR = document.getElementById('new-rat');
 const easterEgg = document.createElement('p');
+
 // Set attributes or properties for the new element
 easterEgg.textContent = 'WELCOME TO THE RAT CULT ;)';
 easterEgg.style.color = '#6f2079';
 easterEgg.style.fontSize = 'x-large';
 easterEgg.style.fontWeight = 'bold';
-
-// Get a reference to the container where you want to insert the new element
 
 // Append the new element to the container
 container.appendChild(easterEgg);
