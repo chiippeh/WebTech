39ec86f33d75eacbb27ca3331a88f9e6a232bdcf
Manu <jourdan.manu@gmail.com>
2023-10-11 13:20:08 +0200

    add student id session

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-    // define("HOSTNAME", "cs3-dev.ict.ru.ac.za");
-    // define("USERNAME", "G21D6937");
-    // define("PASSWORD", "G21D6937");
-    // define("SCHEMA", "team3");
+    define("HOSTNAME", "cs3-dev.ict.ru.ac.za");
+    define("USERNAME", "G21D6937");
+    define("PASSWORD", "G21D6937");
+    define("SCHEMA", "team3");
 
-    define("HOSTNAME", "localhost");
-    define("USERNAME", "root");
-    define("PASSWORD", "");
-    define("SCHEMA", "webtech");
+    // define("HOSTNAME", "localhost");
+    // define("USERNAME", "root");
+    // define("PASSWORD", "");
+    // define("SCHEMA", "webtech");
 ?>
